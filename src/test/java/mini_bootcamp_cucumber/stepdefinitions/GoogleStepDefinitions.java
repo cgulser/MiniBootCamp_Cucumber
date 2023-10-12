2d02f0dcd447202be5abf6e0224f5f06059571ef
Cevdet Gulser <c.gulser86@gmail.com>
2023-10-12 21:10:01 +0200

    12.10.2023

--- src/test/java/mini_bootcamp_cucumber/stepdefinitions/GoogleStepDefinitions.java
+++ src/test/java/mini_bootcamp_cucumber/stepdefinitions/GoogleStepDefinitions.java
@@ -7,15 +7,20 @@ import io.cucumber.java.en.When;
 import mini_bootcamp_cucumber.pages.GooglePage;
 import mini_bootcamp_cucumber.utilities.ConfigurationReader;
 import mini_bootcamp_cucumber.utilities.Driver;
+import mini_bootcamp_cucumber.utilities.ReusableMethods;
 import org.junit.Assert;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GoogleStepDefinitions {
 
     GooglePage googlePage = new GooglePage();
     @Given("kullanici googlea gider")
     public void kullaniciGoogleaGider() {
-        Driver.getDriver().get(ConfigurationReader.getProperty("google_url"));
+        WebDriver driver = Driver.getDriver();
+        driver.get(ConfigurationReader.getProperty("google_url"));
+
     }
 
     @When("togg icin arama yapar")
